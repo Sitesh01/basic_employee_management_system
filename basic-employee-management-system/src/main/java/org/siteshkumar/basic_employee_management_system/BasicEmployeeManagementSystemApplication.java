7e3b3795dd1627146d64e60591eb54ff5e07b8dc
Sitesh01 <siteshisunique1998@gmail.com>
2025-06-07 12:25:13 +0530

    first commit

---
+++ basic-employee-management-system/src/main/java/org/siteshkumar/basic_employee_management_system/BasicEmployeeManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package org.siteshkumar.basic_employee_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicEmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Sitesh Kumar");
+		SpringApplication.run(BasicEmployeeManagementSystemApplication.class, args);
+		System.out.println("This project is developed by Sitesh Kumar");
+	}
+}
---
+++ basic-employee-management-system/src/test/java/org/siteshkumar/basic_employee_management_system/BasicEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.siteshkumar.basic_employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
