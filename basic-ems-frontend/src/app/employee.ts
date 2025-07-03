e3bee5b85f60d5d381ef921151b7ed2e5b188f71
Sitesh01 <siteshisunique1998@gmail.com>
2025-07-03 15:09:09 +0530

    fifth commit

--- basic-ems-frontend/src/app/employee.ts
+++ basic-ems-frontend/src/app/employee.ts
@@ -1,2 +1,7 @@
 export class Employee {
+    id : number | undefined;
+    firstName : String | undefined;
+    lastName : String | undefined;
+    emailId : String | undefined;
+    phone : String | undefined;
 }
